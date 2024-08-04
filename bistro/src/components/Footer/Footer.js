92f65281e423fca2dc21e435acfc506ceec11bde
Mariklimova <mariku22@yandex.ru>
2024-08-04 09:07:57 +0300

    added footer

--- bistro/src/components/Footer/Footer.js
+++ bistro/src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
         <div className={style.footer_color}>
             <div className={style.footer_wrapper}>
 
-                <div className={style.col_left}>
+                <div className={style.column_left}>
                     <div className={style.footer_logo}>
                         <div className={style.white_logo}></div>
                         <h1>Bistro Bliss</h1>
@@ -14,10 +14,32 @@ export default function Footer() {
                     <p>In the new era of technology we look a in the future with certainty and pride to for our company and.</p>
                 </div>
 
-                <div className={style.col_center}></div>
+                <div className={style.column_center}>
+                    <div className={style.column_center_text}>
+                        <h2>Pages</h2>
+                        {['Home', 'About', 'Menu', 'Pricing', 'Blog', 'Contact', 'Delivery'].map((el, i) => <p key={i}>{el}</p>)}
+                    </div>
+                    <div className={style.column_center_text}>
+                        <h2>Utility Pages</h2>
+                        {['Start Here', 'Styleguide', 'Password Protected', '404 Not Found', 'Licenses', 'Changelog', 'View More'].map((el, i) =>
+                            <p key={i}>{el}</p>)}
+                    </div>
+                </div>
+
+                <div className={style.column_right}>
+                    <h2>Follow Us On Instagram</h2>
+                    <div className={style.images}>
+
+                        {[style.img1, style.img2, style.img3, style.img4].map((elem, index) =>
+                            <div key={index} className={elem}></div>)}
+                    </div>
+                </div>
             </div>
+
+                <div className={style.line}></div>
+                <p className={style.hashtag}>Copyright © 2023 Hashtag Developer. All Rights Reserved</p>
         </div>
 
 
-    </footer>
+    </footer >
 }
