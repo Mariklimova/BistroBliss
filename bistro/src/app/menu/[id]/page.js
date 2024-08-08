290157713b6fb5a32d0b6952aa0b55338a1e28c4
Mariklimova <mariku22@yandex.ru>
2024-08-09 00:10:20 +0300

    working on the menu item page

--- bistro/src/app/menu/[id]/page.js
+++ bistro/src/app/menu/[id]/page.js
@@ -26,10 +26,59 @@ export default function Id() {
                 <div className={style.text_item}>
                     <h1>{data.title}</h1>
                     <h2>{data.description}</h2>
-                    <p>Total:{data.price}</p>
+                    <p>Size</p>
+                    <div className={style.buttons_wrapper}>
+
+                        <button className={style.count_button}>
+
+                            <p className={style.size}>S</p>
+                            <p className={style.count}>1s</p>
+                        </button>
+
+                        <button className={style.count_button}>
+
+                            <p className={style.size}>M</p>
+                            <p className={style.count}>2s</p>
+                        </button>
+
+                        <button className={style.count_button}>
+
+                            <p className={style.size}>L</p>
+                            <p className={style.count}>3s</p>
+                        </button>
+
+                        <button className={style.count_button}>
+
+                            <p className={style.size}>XL</p>
+                            <p className={style.count}>4s</p>
+                        </button>
+
+                    </div>
+
+                    <div className={style.total}>
+                        <div className={style.price}>
+                            <p>Total:</p>
+                            <p>{data.price}</p>
+                        </div>
+
+                        <div className={style.line}></div>
+                        <div className={style.text_description}>
+                            <div className={style.img_info}></div>
+                            <p>The cost is not final. Download our mobile app to see the final price and place your order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.</p>
+                        </div>
+                        <button className={style.add}>Add to Order</button>
+                    </div>
                 </div>
             </div>
         </div>
         <Footer />
     </>
+
 }
+
+
+// {['S','M','L','XL'].map((el)=><div className={style.count}>
+//
+//     <p>{el}</p>
+//{['1s','2s','3s','4s'].map((elem)=><p className={style.count_text}>{elem}</p>)}
+// </div>)}
