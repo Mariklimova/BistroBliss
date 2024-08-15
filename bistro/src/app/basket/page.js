c1a6c280d8e0a4b1e3c37ba53df49bcfbc387d3a
Mariklimova <mariku22@yandex.ru>
2024-08-15 21:05:28 +0300

    added funnctionality

--- bistro/src/app/basket/page.js
+++ bistro/src/app/basket/page.js
@@ -18,28 +18,39 @@ export default function Basket() {
 
     const deleteBasketItems = (elem) => {
         const delItems = basketFood.filter((el) => el.id !== elem.id);
-        localStorage.setItem('basketArr',JSON.stringify([...delItems,elem]));
+
+        localStorage.setItem('basketArr', JSON.stringify(delItems, elem));
         setBasketFood([...delItems])
+    }
+
+    const clearBasket = () => {
+        localStorage.setItem('basketArr', JSON.stringify([]))
+        setBasketFood([])
     }
 
     return <>
         <Header />
+        <div style ={{maxWidth:1293,margin:'auto'}} >
         <h1 className={style.title_basket}>Basket</h1>
         <div className={style.container}>
-            {basketFood.map((elem, i) =>
-                <div key={i} className={style.item} onClick={()=>deleteBasketItems(elem)}>
-                    <div className={style[elem.img]}></div>
-                    <div className={style.text_container}>
-                        <p className={style.price}>{elem.price}</p>
-                        <h2>{elem.title}</h2>
-                        <p>{elem.description}</p>
+            {basketFood.length ?
+                basketFood.map((elem, i) =>
+                    <div key={i} className={style.item} onClick={() => deleteBasketItems(elem)}>
+                        <div className={style[elem.img]}></div>
+                        <div className={style.text_container}>
+                            <p className={style.price}>{elem.price}</p>
+                            <h2>{elem.title}</h2>
+                            <p>{elem.description}</p>
 
-                        <button className={style.btn_delete}>Delete</button>
+                            <button className={style.btn_delete}>Delete</button>
+                        </div>
+
                     </div>
 
-                </div>
+                ) : <p>Your basket is empty</p>}
+        </div>
+        <button className={style.btn_delete_all} onClick={()=>clearBasket()}>Clear the basket</button>
 
-            )}
         </div>
         <Footer />
     </>
--- bistro/src/app/menu/page.js
+++ bistro/src/app/menu/page.js
@@ -10,15 +10,20 @@ import { useEffect, useState } from 'react';
 
 export default function Menu() {
 
-    const arrSizeFood2 = [
-        { img_style: style.img_drinks, count: 'Lemonade' },
-        { img_style: style.img_mainFood, count: 'Main courses' },
-        { img_style: style.img_desserts, count: 'Dessert' }];
+    const arrCategoryButtons = [
+        { img_btn: 'img_all', count: 'All' },
+        { img_btn: 'img_drinks', count: 'Lemonade' },
+        { img_btn: 'img_mainFood', count: 'Main courses' },
+        { img_btn: 'img_desserts', count: 'Dessert' }];
 
-    const [activCategory, setActivCategory] = useState('Lemonade');
+    const [activCategory, setActivCategory] = useState('All');
     const [food, setFood] = useState([]);
 
     useEffect(() => {
+        if(activCategory=='All'){
+            setFood(ourMenuArr);
+            return;
+        }
         const result = ourMenuArr.filter((el) => el.category == activCategory)
         setFood(result)
         console.log(result);
@@ -30,24 +35,25 @@ export default function Menu() {
         <div className={style.menu_wrapper}>
             <div className={style.menu_title}>
                 <h1>Our Menu</h1>
+                <p>We consider all the drivers of change gives you the components you need to change to create a truly happens.</p>
                 <div className={style.buttons_wrapper}>
 
-                    {arrSizeFood2.map((el,i) =>
-                        <button key={i} className={style.count_button} onClick={()=>setActivCategory(el.count)}>
-
-                            <div className={el.img_style}></div>
+                    {arrCategoryButtons.map((el, i) =>
+                        <button key={i} className={style.count_button} onClick={() => setActivCategory(el.count)}>
+                            <div className={style.wrapper_img} >
+                                <div className={style[el.img_btn]}></div>
+                            </div>
                             <p className={style.count}>{el.count}</p>
                         </button>
 
                     )}
                 </div>
-                <p>We consider all the drivers of change gives you the components you need to change to create a truly happens.</p>
             </div>
             <div className={style.container}>
                 {food.map((elem, id) => <Link href={`/menu/${elem.id}`} key={id}>
                     <div className={style.item}>
-                        <div className={style[elem.img]}></div>
                         <div className={style.text_container}>
+                            <div className={style[elem.img]}></div>
                             <p className={style.price}>{elem.price}</p>
                             <h2>{elem.title}</h2>
                             <p>{elem.description}</p>
